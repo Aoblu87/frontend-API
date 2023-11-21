0a37f64a942ea9980fad59676e90b26dab243ca2
Stefania <stefania.struzzi@gmail.com>
2023-11-21 21:23:00 +0100

    creata repo del front end es api in express. creato primo collegamento

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import NavBar from "./components/navbar/BlogNavbar";
+import Footer from "./components/footer/Footer";
+import Home from "./views/home/Home";
+import Blog from "./views/blog/Blog";
+import NewBlogPost from "./views/new/New";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  const makeAPICall = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/cors', {mode:'cors'});
+      const data = await response.json();
+      console.log({ data })
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+  useEffect(() => {
+    makeAPICall();
+  }, [])
+
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/new" element={<NewBlogPost />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
