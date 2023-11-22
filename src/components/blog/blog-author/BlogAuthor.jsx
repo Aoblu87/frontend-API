19b6b3dffdf2276bb701d26f2402a15de25968eb
Stefania <stefania.struzzi@gmail.com>
2023-11-22 18:43:11 +0100

    creata fetch che comunica con il server backend

--- src/components/blog/blog-author/BlogAuthor.jsx
+++ src/components/blog/blog-author/BlogAuthor.jsx
@@ -1,20 +1,52 @@
-import React from "react";
-import { Col, Image, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Col, Image, Row, Spinner } from "react-bootstrap";
 import "./styles.css";
 
-const BlogAuthor = props => {
-  const { name, avatar } = props;
-  return (
+export default function BlogAuthor() {
+  // const { name, avatar } = props;
+  const [authors, setAuthors] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const makeAPICall = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:3001/api/authors/655cf8a511702bc83d63dc86"
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setAuthors(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    makeAPICall();
+  }, []);
+
+  return loading ? (
+    <div className="d-flex mt-5">
+      <Spinner animation="border" variant="primary" className="mx-auto" />{" "}
+    </div>
+  ) : (
     <Row>
-      <Col xs={"auto"} className="pe-0">
-        <Image className="blog-author" src={avatar} roundedCircle />
-      </Col>
-      <Col>
-        <div>di</div>
-        <h6>{name}</h6>
-      </Col>
+      {/* {authors.map((author) => ( */}
+      <>
+        <Col xs={"auto"} className="pe-0" key={authors._id}>
+          <Image className="blog-author" src={authors.avatar} roundedCircle />
+        </Col>
+
+        <Col className="d-flex align-items-center">
+          <h6>di {authors.nome}</h6>
+        </Col>
+      </>
+      {/* ))} */}
     </Row>
   );
-};
-
-export default BlogAuthor;
+}
