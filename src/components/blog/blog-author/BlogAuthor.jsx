b98e9344adcf6ad9278d39a0ff4f74622dae1bfc
Stefania <stefania.struzzi@gmail.com>
2023-11-23 15:14:29 +0100

    sistemato prop per fisualizzare autore su card home

--- src/components/blog/blog-author/BlogAuthor.jsx
+++ src/components/blog/blog-author/BlogAuthor.jsx
@@ -2,33 +2,8 @@ import React from "react";
 import { Col, Image, Row, Spinner } from "react-bootstrap";
 import "./styles.css";
 
-export default function BlogAuthor(loading, author) {
-  // const { name, avatar } = props;
-  // const [authors, setAuthors] = useState([]);
-  // const [loading, setLoading] = useState(true);
-
-  // const makeAPICall = async () => {
-  //   try {
-  //     const response = await fetch(
-  //       "http://localhost:3001/api/authors/655f184e8f9470487ace52d4"
-  //     );
-
-  //     if (!response.ok) {
-  //       throw new Error(`HTTP error! Status: ${response.status}`);
-  //     }
-
-  //     const data = await response.json();
-  //     setAuthors(data);
-  //   } catch (error) {
-  //     console.error("Error fetching data:", error);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   makeAPICall();
-  // }, []);
+export default function BlogAuthor(props) {
+  const { name, avatar, loading, _id } = props;
 
   return loading ? (
     <div className="d-flex mt-5">
@@ -36,17 +11,15 @@ export default function BlogAuthor(loading, author) {
     </div>
   ) : (
     <Row>
-      {/* {authors.map((author) => ( */}
       <>
-        <Col xs={"auto"} className="pe-0" key={author._id}>
-          <Image className="blog-author" src={author.avatar} roundedCircle />
+        <Col xs={"auto"} className="pe-0" key={_id}>
+          <Image className="blog-author" src={avatar} roundedCircle />
         </Col>
 
         <Col className="d-flex align-items-center">
-          <h6>di {author.nome}</h6>
+          <h6>di {name}</h6>
         </Col>
       </>
-      {/* ))} */}
     </Row>
   );
 }
