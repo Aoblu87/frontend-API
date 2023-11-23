7f08a8db547cd975bbab73af3fcd05f29268c119
Stefania <stefania.struzzi@gmail.com>
2023-11-23 21:20:27 +0100

    creata barra ricerca nella navbar

--- src/views/new/New.jsx
+++ src/views/new/New.jsx
@@ -3,11 +3,60 @@ import { Button, Container, Form } from "react-bootstrap";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "./styles.css";
-const NewBlogPost = props => {
+const NewBlogPost = (props) => {
   const [text, setText] = useState("");
-  const handleChange = useCallback(value => {
+  const handleChange = useCallback((value) => {
     setText(value);
   });
+
+  //     const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     setLoading(true);
+  //     const formData ={
+
+  //         readTime{
+  //             value,
+  //             unit,
+  //         },
+  //         author {
+  //             name,
+  //             avatar
+  //         },
+  //         _id,
+  //         category,
+  //         title,
+  //         cover,
+  //         content,
+  //       }
+
+  //     const addBlog = async () => {
+  //       try {
+  //         const response = await fetch("http://localhost:3001/api/blogPosts/", {
+  //           headers: {
+  //             "Content-Type": "application/json",
+  //           },
+  //           method: "POST",
+  //           body: JSON.stringify(formData),
+  //         });
+
+  //         if (!response.ok) {
+  //           throw new Error(`HTTP error! Status: ${response.status}`);
+  //         }
+
+  //         const data = await response.json();
+  //         setBlog(data);
+  //       } catch (error) {
+  //         console.error("Error fetching data:", error);
+  //       } finally {
+  //         setLoading(false);
+  //       }
+  //     };
+
+  //     useEffect(() => {
+  //       addBlog();
+  //     }, []);
+  //   })
+  // }
   return (
     <Container className="new-blog-container">
       <Form className="mt-5">
@@ -27,7 +76,11 @@ const NewBlogPost = props => {
         </Form.Group>
         <Form.Group controlId="blog-content" className="mt-3">
           <Form.Label>Contenuto Blog</Form.Label>
-          <ReactQuill value={text} onChange={handleChange} className="new-blog-content" />
+          <ReactQuill
+            value={text}
+            onChange={handleChange}
+            className="new-blog-content"
+          />
         </Form.Group>
         <Form.Group className="d-flex mt-3 justify-content-end">
           <Button type="reset" size="lg" variant="outline-dark">
